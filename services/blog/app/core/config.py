23be064caec460a0e5b0ef31fdd1cee952776135
Ntale Samad <swamaduntale98@gmail.com>
2026-05-29 22:08:59 +0300

    fix: remove empty quotes on cloudinary config

--- services/blog/app/core/config.py
+++ services/blog/app/core/config.py
@@ -8,9 +8,9 @@ class Settings(BaseSettings):
     USER_SERVICE_URL: str = "http://user_service:8002"
 
     # Cloudinary
-    CLOUDINARY_CLOUD_NAME: str = ""
-    CLOUDINARY_API_KEY:    str = ""
-    CLOUDINARY_API_SECRET: str = ""
+    CLOUDINARY_CLOUD_NAME: str 
+    CLOUDINARY_API_KEY:    str
+    CLOUDINARY_API_SECRET: str
 
     class Config:
         env_file = ".env"
